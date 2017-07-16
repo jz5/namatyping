1b39ddd0ad3c46d2c5f03322e4e7eda6915a02c6
100の人 <esperecyan@users.noreply.github.com>
2017-07-16 14:40:02 +0900

    ImageSourceConverter が null を処理できない .NET Framework の問題に対処

---
+++ NamaTyping/Converter/NullToUnsetValueConverter.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Globalization
+
+Namespace Converter
+
+    ''' <summary>
+    ''' <c>Nothing</c>を<see cref="DependencyProperty.UnsetValue"/>へ変換します。
+    ''' </summary>
+    ''' <seealso cref="ImageSourceConverter"/>
+    Public Class NullToUnsetValueConverter
+        Implements IValueConverter
+
+        Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Return If(value, DependencyProperty.UnsetValue)
+        End Function
+
+        Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return Binding.DoNothing
+        End Function
+
+    End Class
+
+End Namespace
