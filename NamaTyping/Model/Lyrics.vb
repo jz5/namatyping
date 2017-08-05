010e3e2ee10376d6012769f882f81909769c24df
100の人 <esperecyan@users.noreply.github.com>
2017-08-06 07:53:07 +0900

    ニコ生タイピング用置換ファイル (*.repl.txt) を読み込む際、検索文字列をトリミング前に検証していた問題を修正
    本コミットにより、検索文字列が空白文字の場合、歌詞ファイル読み込み時にクラッシュする不具合も修正された。

--- NamaTyping/Model/Lyrics.vb
+++ NamaTyping/Model/Lyrics.vb
@@ -306,11 +306,17 @@ Namespace Model
                     Continue For
                 End If
 
-                values(0) = values(0).ToLyricsWords(False)
+                values(0) = values(0).Trim().ToLyricsWords(False)
+                If values(0) = "" Then
+                    ' 検索文字列が空白文字のみで構成されていれば
+                    ' 無効
+                    Continue For
+                End If
+
                 If Not ReplacementWords.ContainsKey(values(0)) Then
                     values(1) = values(1).ToLyricsWords(True)
                     If values(1) <> "" Then
-                        ReplacementWords.Add(values(0).Trim, values(1))
+                        ReplacementWords.Add(values(0), values(1))
                     End If
                 Else
                     ' MEMO: 風クスの同じ単語がある場合
