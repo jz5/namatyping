f25b6d764ec6cfa2d105a9f37984c74b94ccc392
100の人 <100@pokemori.jp>
2018-02-03 15:14:30 +0900

    ランキング表示にかかる時間の上限を20秒にした
    コメントしたユーザーが多い場合、非常に時間がかかっていたため。

--- NamaTyping/ViewModel/MainViewModel.vb
+++ NamaTyping/ViewModel/MainViewModel.vb
@@ -1103,8 +1103,16 @@ Namespace ViewModel
                 Next
             End If
 
+            ' 表示時間の調整
+            Dim spanSeconds = 1.0
+            Dim durationSenconds = orderedMember.Count * spanSeconds
+            If durationSenconds > My.Settings.DurationSecondsLimitDisplayingRanking Then
+                durationSenconds = My.Settings.DurationSecondsLimitDisplayingRanking
+                spanSeconds = durationSenconds / orderedMember.Count
+            End If
+
             If Player.HasAudio Then
-                Dim p = Player.NaturalDuration.TimeSpan.TotalSeconds - (orderedMember.Count + 1)
+                Dim p = Player.NaturalDuration.TimeSpan.TotalSeconds - (spanSeconds * orderedMember.Count + 1)
                 If p < 0 Then
                     p = 0
                 End If
@@ -1114,7 +1122,7 @@ Namespace ViewModel
                 Player.Play()
             End If
 
-            RankingTimer.Interval = New TimeSpan(0, 0, 1)
+            RankingTimer.Interval = TimeSpan.FromSeconds(spanSeconds)
             RankingTimer.Start()
 
             '#If DEBUG Then
